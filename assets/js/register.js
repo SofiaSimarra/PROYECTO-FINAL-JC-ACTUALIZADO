172141258dde2322cdc309a967277faee500884e
SofiaSimarra <simarra2008@outlook.com>
2024-11-21 21:53:52 -0500

    Ya hemos completado la página.

--- assets/js/register.js
+++ assets/js/register.js
@@ -25,16 +25,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// El document.addEventListener('DOMContentLoaded', () => { ... }) se utiliza para asegurarse de que el documento HTML esté completamente cargado antes de ejecutarse.
-
-// El DOMContentLoaded es un evento que se activa cuando el html ha cargado completamente y el DOM está listo para ser manipulado.
-
-// El const registerForm = document.getElementById('register-form'); se utiliza para seleccionar el formulario de registro en el html usando su ID.
-
-// El registerForm.addEventListener('submit', (e) => { ... }); este bloque está esperando a que se envie el formulario de registro para ejecutar la funcion dentro del bloque.
-
-// El submit es el evento que se activa cuando se envia el formulario.
-
-// El e.preventDefault(); Este evento evita el la accion predeterminada de los formularios, la cual generalmente es enviar los datos y recargar la pagina, en este caso se utiliza para evitar que se recargue la pagina y poder hacer la validacion.
-
-// El JSON.parse() permite convertir los datos guardados como texto en un pbjeto o arreglo, en caso de que no hayan se crea un arreglo vacio.
+// Espera a que la pagina este completamente cargada, recoge los datos del formulario, guarda en el local storage y lleva al usuario a la pagina de inicio de sesion.
